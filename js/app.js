a05ca4af0f6528e1b52ae16b5a24bcd606a9127e
S_Techieweb <86921538+S-Techieweb@users.noreply.github.com>
2021-07-31 13:17:20 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ function sendmail(){
 
   Email.send({
         SecureToken:"fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
-    To: 'shivatechieweb@gmail.com',
+    To: 'stechieweb@gmail.com',
     From: " S-Techieweb@gmail.com",
     Subject: "New message "+name,
     Body: Body
